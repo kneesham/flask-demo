16b35f414234c5565709e3123360ee1c11ccb8a1
Ted Nesham <theodore.nesham@gmail.com>
2025-01-11 00:41:54 -0600

    initial actions change.

---
+++ Routes/base.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+@app.route('/', methods=['POST'])
+def receive_data():
+    if request.method == 'POST':
+        # Access the data sent in the POST request
+        data = request.get_json()  # For JSON data
+        # data = request.form  # For form data
+
+        # Process the data (e.g., save to database, perform calculations)
+        print(data)
+
+        # Return a response
+        return data
